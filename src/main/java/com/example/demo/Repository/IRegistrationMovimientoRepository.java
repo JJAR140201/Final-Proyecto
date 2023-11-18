d7c39695badce0f38da113c46bd1c43f3bab802e
JJAR140201 <avilesroajuanjose@gmail.com>
2023-11-18 10:25:36 -0500

    Creamos IRegistrationMovimientoRepository.java

---
+++ src/main/java/com/example/demo/Repository/IRegistrationMovimientoRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Employed;
+import com.example.demo.Entity.RegistrationMovimiento;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface IRegistrationMovimientoRepository extends IBaseRepository<RegistrationMovimiento, Long> {
+    @Query("SELECT r FROM RegistroMovimiento r WHERE r.dateEntry = :dateEntry")
+    List<RegistrationMovimiento> findByDateEntry(@Param("dateEntry") LocalDateTime dateEntry);
+
+    @Query("SELECT r FROM RegistroMovimiento r WHERE r.dateOutput = :dateOutput")
+    List<RegistrationMovimiento> findByDateOutput(@Param("dateOutput") LocalDateTime dateOutput);
+}
