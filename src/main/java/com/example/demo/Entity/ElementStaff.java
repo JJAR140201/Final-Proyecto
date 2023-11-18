a97fbbd694ba4f7867ac9a9a544d7fff09161ad0
JJAR140201 <avilesroajuanjose@gmail.com>
2023-11-18 09:49:19 -0500

    Creamos la clase elemento de empleado

---
+++ src/main/java/com/example/demo/Entity/ElementStaff.java
@@ -0,0 +1,31 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.apachecommons.CommonsLog;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "ElementStaff")
+public class ElementStaff extends Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "nameObject")
+    public String nameObject;
+
+    @Column(name = "description")
+    public String description;
+
+    @Column(name = "dateAcquisition")
+    public LocalDateTime dateAcquisition;
+}
