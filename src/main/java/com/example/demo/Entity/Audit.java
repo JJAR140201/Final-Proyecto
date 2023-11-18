9f87100888e5b6839132ee89b6e86b789ce069d7
JJAR140201 <avilesroajuanjose@gmail.com>
2023-11-18 10:38:01 -0500

    Agregamos getter and setter

--- src/main/java/com/example/demo/Entity/Audit.java
+++ src/main/java/com/example/demo/Entity/Audit.java
@@ -6,12 +6,6 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
 public abstract class Audit {
 
     @Id
@@ -38,4 +32,68 @@ public abstract class Audit {
 
     @Column(name = "deleted_at", nullable = true)
     private LocalDateTime deletedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the state
+     */
+    public Boolean getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the createdAt
+     */
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * @param createdAt the createdAt to set
+     */
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * @return the updatedAt
+     */
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    /**
+     * @param updatedAt the updatedAt to set
+     */
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    /**
+     * @return the deletedAt
+     */
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    /**
+     * @param deletedAt the deletedAt to set
+     */
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
