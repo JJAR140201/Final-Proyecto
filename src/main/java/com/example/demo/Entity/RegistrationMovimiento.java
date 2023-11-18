6745375a1b5260dcf3500d68157ef9063286b209
JJAR140201 <avilesroajuanjose@gmail.com>
2023-11-18 09:52:00 -0500

    Creamos entidad de RegistrationMovimiento

---
+++ src/main/java/com/example/demo/Entity/RegistrationMovimiento.java
@@ -0,0 +1,27 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "RegistrationMovimiento")
+public class RegistrationMovimiento extends Audit{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "dateEntry")
+    public LocalDateTime dateEntry;
+
+    @Column(name = "dateOutput")
+    public LocalDateTime dateOutput;
+}
