5bd124e50a96af6100b54fbf0397f24232e43818
JJAR140201 <avilesroajuanjose@gmail.com>
2023-11-22 09:32:31 -0500

    Cambios parcial 3

--- src/main/java/com/example/demo/Controller/EmployedController.java
+++ src/main/java/com/example/demo/Controller/EmployedController.java
@@ -49,7 +49,7 @@ public class EmployedController extends BaseController<Employed, IEmployedServic
         return new ResponseEntity<>(employedList, HttpStatus.OK);
     }
 
-    @GetMapping("/findEmployeeDetailsById/{id}")
+    @GetMapping("/findEmployeeDetailsById/{employedId}")
     public ResponseEntity<IEmployeeDetails> findEmployeeDetailsById(@PathVariable Long employedId) {
         IEmployeeDetails employeeDetails = (IEmployeeDetails) employedService.findEmployeeDetailsById(employedId);
         return new ResponseEntity<>(employeeDetails, HttpStatus.OK);
